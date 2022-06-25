19aeea07059134286988028494af9f2f2cf3e042
RinKit6024 <butasan6024@gmail.com>
2022-06-25 16:14:26 +0900

    update

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -43,7 +43,6 @@ public class GamePanel extends JPanel implements ActionListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
-		
 	}
 	
 	public void draw(Graphics g) {
@@ -53,21 +52,48 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		g.setColor(Color.red);
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
+		for(int i = 0; i< bodyParts; i++) {
+			if(i == 0) {
+				g.setColor(Color.green);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				
+			}else {
+				g.setColor(new Color(45,180,0));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+		}
 	}
 	
 	public void move() {
-
+		for (int i = bodyParts; i>0 ; i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		
+		switch(direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+		
+		
+		}
 	}
 	
 	public void newApple() { //アイテム取得した際のアクション
 		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-		appleY = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 		System.out.println(appleX);
 		System.out.println(appleY);
 	}
 	
 	public void checkApple() { //アイテム取得した際のアクション
-		
+
 	}
 	
 	public void checkCollisions() { //コリジョンしたとき用のアクション
@@ -82,7 +108,12 @@ public class GamePanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 	}
 	
 	public class MyKeyAdapter extends KeyAdapter {
